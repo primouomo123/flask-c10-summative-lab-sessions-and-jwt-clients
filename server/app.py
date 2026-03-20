71977a21f016767404976ff634a6749af260f5a5
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-03-19 21:42:29 -0400

    Main setup complete

---
+++ server/app.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+from flask import request, session, jsonify, make_response
+from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
+from flask_jwt_extended import create_access_token, get_jwt_identity, verify_jwt_in_request
+
+from config import app, db, api, jwt
+from models import User, Recipe, UserSchema, RecipeSchema
+
+@app.before_request
+def check_if_logged_in():
+    open_access_list = [
+        'signup',
+        'login'
+    ]
+
+    if (request.endpoint) not in open_access_list and (not verify_jwt_in_request()):
+        return {'error': '401 Unauthorized'}, 401
+
+class Signup(Resource):
+    def post(self):
+
+        request_json = request.get_json()
+
+        username = request_json.get('username')
+        password = request_json.get('password')
+        image_url = request_json.get('image_url')
+        bio = request_json.get('bio')
+
+        user = User(
+            username=username,
+            image_url=image_url,
+            bio=bio
+        )
+        user.password_hash = password
+        
+        try:
+            db.session.add(user)
+            db.session.commit()
+            access_token = create_access_token(identity=str(user.id))
+            return make_response(jsonify(token=access_token, user=UserSchema().dump(user)), 200)
+        except IntegrityError:
+            return {'errors': ['422 Unprocessable Entity']}, 422
+
+class WhoAmI(Resource):
+    def get(self):
+        user_id = get_jwt_identity()
+            
+        user = User.query.filter(User.id == user_id).first()
+        
+        return UserSchema().dump(user), 200
+
+
+class Login(Resource):
+    def post(self):
+
+        username = request.json['username']
+        password = request.json['password']
+
+        user = User.query.filter(User.username == username).first()
+
+        if user and user.authenticate(password):
+            token = create_access_token(identity=str(user.id))
+            return make_response(jsonify(token=token, user=UserSchema().dump(user)), 200)
+
+        return {'errors': ['401 Unauthorized']}, 401
+
+api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(WhoAmI, '/me', endpoint='me')
+api.add_resource(Login, '/login', endpoint='login')
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
