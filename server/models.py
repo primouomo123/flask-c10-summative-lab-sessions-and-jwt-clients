6f269123009ab747ad3782035df7db7a27c4ddad
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-03-19 21:56:05 -0400

    User model created

---
+++ server/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import CheckConstraint
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.hybrid import hybrid_property
+from marshmallow import Schema, fields, validates_schema, ValidationError
+
+from config import db, bcrypt
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    _password_hash = db.Column(db.String(128), nullable=True)
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("Password hashes may not be viewed.")
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+    
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
